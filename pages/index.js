e4cd551f5676bed6fbff07c905d756de93bc94ac
grigarazH <45718270+grigarazH@users.noreply.github.com>
2021-11-02 22:00:26 +0300

    Добавил кнопку удаления

--- pages/index.js
+++ pages/index.js
@@ -25,104 +25,100 @@ const initialCards = [
   }
 ];
 
+const cardContainer = document.querySelector(".cards");
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfilePopup = document.querySelector(".popup_type_edit-profile");
+const editProfileCloseButton = editProfilePopup.querySelector(".popup__close-button");
+const editProfileForm = editProfilePopup.querySelector(".popup__container");
+const cardTemplate = document.querySelector("#card_template");
+const editProfileNameInput = editProfilePopup.querySelector("#edit_profile_name");
+const editProfileSubtitleInput = editProfilePopup.querySelector("#edit_profile_subtitle");
+const addCardButton = document.querySelector(".profile__add-button");
+const addCardPopup = document.querySelector(".popup_type_add-card");
+const addCardCloseButton = addCardPopup.querySelector(".popup__close-button");
+const addCardForm = addCardPopup.querySelector(".popup__container");
+const profileNameEl = document.querySelector(".profile__name");
+const profileSubtitleEl = document.querySelector(".profile__subtitle");
+const addCardNameInput = addCardForm.querySelector("#add_card_name");
+const addCardLinkInput = addCardForm.querySelector("#add_card_link");
+
 let profileName = document.querySelector(".profile__name").textContent;
 let profileSubtitle = document.querySelector(".profile__subtitle").textContent;
 
 //Принимает объект карточки с параметрами link и name. Возвращает html-элемент карточки.
 function getCardElement(card) {
-  let cardTemplate = document.querySelector("#card_template");
-  let cardElement = cardTemplate.content.firstElementChild.cloneNode(true);
-  let cardImage = cardElement.querySelector(".card__image");
-  let cardDescription = cardElement.querySelector(".card__title");
+  const cardElement = cardTemplate.content.firstElementChild.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardDescription = cardElement.querySelector(".card__title");
   cardImage.src = card.link;
   cardImage.alt = `Фотография места ${card.name}`;
   cardDescription.textContent = card.name;
   return cardElement;
 }
 
-let cardContainer = document.querySelector(".cards");
-
-initialCards.forEach(card => {
-  cardContainer.appendChild(getCardElement(card));
-});
-
 function likeButtonToggle(button) {
   button.classList.toggle("card__like-button_active");
 }
-
-let cards = cardContainer.querySelectorAll(".card");
-cards.forEach(card => {
-  let likeButton = card.querySelector(".card__like-button");
-  likeButton.addEventListener("click", function(){
-    likeButtonToggle(likeButton);
-  });
-});
 
 function togglePopup(popup) {
   popup.classList.toggle("popup_opened");
 }
 
-let editProfileButton = document.querySelector(".profile__edit-button");
-let editProfilePopup = document.querySelector(".popup_type_edit-profile");
+//Данная функция вызывается при рендеринге страницы
+function render() {
+  profileNameEl.textContent = profileName;
+  profileSubtitleEl.textContent = profileSubtitle;
+  editProfileNameInput.value = profileName;
+  editProfileSubtitleInput.value = profileSubtitle;
+}
+
+initialCards.forEach(card => cardContainer.append(getCardElement(card)));
+
+const cards = cardContainer.querySelectorAll(".card");
+cards.forEach(card => {
+  const likeButton = card.querySelector(".card__like-button");
+  likeButton.addEventListener("click", function(){
+    likeButtonToggle(likeButton);
+  });
+});
 
 editProfileButton.addEventListener("click", function() {
   togglePopup(editProfilePopup);
 });
-let editProfileCloseButton = editProfilePopup.querySelector(".popup__close-button");
+
 editProfileCloseButton.addEventListener("click", function() {
   togglePopup(editProfilePopup);
 });
-let editProfileForm = editProfilePopup.querySelector(".popup__container");
 
 editProfileForm.addEventListener("submit", function(evt) {
   evt.preventDefault();
-  let editProfileNameInput = editProfilePopup.querySelector("#profile_name");
-  let editProfileSubtitleInput = editProfilePopup.querySelector("#profile_subtitle");
   profileName = editProfileNameInput.value;
   profileSubtitle = editProfileSubtitleInput.value;
   togglePopup(editProfilePopup);
   render();
 });
 
-let addCardButton = document.querySelector(".profile__add-button");
-let addCardPopup = document.querySelector(".popup_type_add-card");
 addCardButton.addEventListener("click", function() {
   togglePopup(addCardPopup);
 });
 
-let addCardCloseButton = addCardPopup.querySelector(".popup__close-button");
 addCardCloseButton.addEventListener("click", function() {
   togglePopup(addCardPopup);
 });
 
-let addCardForm = addCardPopup.querySelector(".popup__container");
 addCardForm.addEventListener("submit", function(evt) {
   evt.preventDefault();
-  let addCardNameInput = addCardForm.querySelector("#add_card_name");
-  let addCardLinkInput = addCardForm.querySelector("#add_card_link");
-  let card = {
+  const card = {
     name: addCardNameInput.value,
     link: addCardLinkInput.value,
   };
-  let cardElement = getCardElement(card);
-  let likeButton = cardElement.querySelector(".card__like-button");
+  const cardElement = getCardElement(card);
+  const likeButton = cardElement.querySelector(".card__like-button");
   likeButton.addEventListener("click", function() {
     likeButtonToggle(likeButton);
   })
   cardContainer.prepend(cardElement);
   togglePopup(addCardPopup);
 })
-
-//Данная функция вызывается при рендеринге страницы
-function render() {
-  let profileNameEl = document.querySelector(".profile__name");
-  let profileSubtitleEl = document.querySelector(".profile__subtitle");
-  profileNameEl.textContent = profileName;
-  profileSubtitleEl.textContent = profileSubtitle;
-  let editProfileNameInput = editProfilePopup.querySelector("#profile_name");
-  let editProfileSubtitleInput = editProfilePopup.querySelector("#profile_subtitle");
-  editProfileNameInput.value = profileName;
-  editProfileSubtitleInput.value = profileSubtitle;
-}
 
 render();
