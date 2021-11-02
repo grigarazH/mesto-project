54e529048a5667c1d871e44d20a489f0652f2d99
grigarazH <45718270+grigarazH@users.noreply.github.com>
2021-11-02 16:08:08 +0300

    Реализовал 6 карточек 'из коробки' и функцию, возвращающую html карточки.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,49 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//Принимает объект карточки с параметрами link и name. Возвращает html карточки.
+function getCardHtml(card) {
+  return `
+    <article class="card">
+    <img class="card__image" src="${card.link}" alt="Фотография места ${card.name}">
+    <div class="card__description">
+      <h2 class="card__title">${card.name}</h2>
+      <button type="button" class="card__like-button"></button>
+      </div>
+    </article>
+  `;
+}
+
+let cardContainer = document.querySelector(".cards");
+
+initialCards.forEach(card => {
+  cardContainer.insertAdjacentHTML("beforeend", getCardHtml(card));
+});
+
 //Данная функция вызывается при рендеринге страницы
 function render() {
     let likeButtons = document.querySelectorAll(".card__like-button");
