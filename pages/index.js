8a852ea02c3a25872a25d5a7428037be8539967c
grigarazH <45718270+grigarazH@users.noreply.github.com>
2021-11-02 15:53:26 +0300

    Реализовал кнопку лайка

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 //Данная функция вызывается при рендеринге страницы
 function render() {
-
+    let likeButtons = document.querySelectorAll(".card__like-button");
+    likeButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            button.classList.toggle("card__like-button_active");
+        });
+    });
 }
 
 render();
