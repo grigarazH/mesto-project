c44dd96150b4e7f2125323f0f458f38bb3b62255
grigarazH <45718270+grigarazH@users.noreply.github.com>
2022-01-23 00:10:13 +0300

    Сделал универсальное закрытие попапа

--- src/components/index.js
+++ src/components/index.js
@@ -14,42 +14,26 @@ import {enableValidation} from "./validate";
 
 const editProfileButton = document.querySelector(".profile__edit-button");
 const addCardButton = document.querySelector(".profile__add-button");
-const addCardCloseButton = popups.addCardPopup.querySelector(".popup__close-button");
-const photoPopupCloseButton = popups.photoPopup.querySelector(".popup__close-button");
-const editProfileCloseButton = popups.editProfilePopup.querySelector(".popup__close-button");
 
 initialCards.forEach(card => cardContainer.append(createCardElement(card)));
 
 editProfileButton.addEventListener("mousedown", e => e.preventDefault());
 addCardButton.addEventListener("mousedown", e => e.preventDefault());
-
 editProfileButton.addEventListener("click", editProfileButtonClick);
-
-editProfileCloseButton.addEventListener("click", () => closePopup(popups.editProfilePopup));
-
 editProfileForm.addEventListener("submit", submitEditProfileForm);
 
 addCardButton.addEventListener("click",()  => {
   openPopup(popups.addCardPopup);
 });
 
-addCardCloseButton.addEventListener("click", () => {
-  closePopup(popups.addCardPopup);
-});
-
 addCardForm.addEventListener("submit", submitAddCardForm);
 
-photoPopupCloseButton.addEventListener("click", () => {
-  closePopup(popups.photoPopup);
-});
-
 Object.keys(popups).forEach(popup => {
-  popups[popup].addEventListener("click", e => {
-    if(e.target.classList.contains("popup")) {
+  popups[popup].addEventListener("mousedown", e => {
+    if(e.target.classList.contains("popup") || e.target.classList.contains("popup__close-button")) {
       closePopup(popups[popup]);
     }
   });
 });
-
 
 enableValidation(validationConfig);
