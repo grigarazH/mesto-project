62adb3a9424ecb674f181d2f4b14d9129f2b6350
grigarazH <45718270+grigarazH@users.noreply.github.com>
2022-01-23 14:32:01 +0300

    Добавил конфиг api

--- src/components/constants.js
+++ src/components/constants.js
@@ -6,3 +6,8 @@ export const validationConfig = {
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_active',
 }
+
+export const apiConfig = {
+  token: '8d39c875-e0dc-4420-9d25-ae2d27971d79',
+  group: 'plus-cohort-6',
+}
