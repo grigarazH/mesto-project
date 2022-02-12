b50ab05219c9d967d8f5ba4817d7c9c725b6ade6
Aleks P <aap-89@yandex.ru>
2022-02-12 10:53:06 +0300

    Добавили класс PopupWithForm

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,28 @@
+import Popup from "./Popup";
+
+export default class PopupWithForm extends Popup {
+  constructor(selector, submitCallback) {
+    super(selector);
+    this._submitCallback = submitCallback;
+  }
+
+  _getInputValues() {
+    const inputValues = {};
+    this._popup = document.querySelector(this._selector);
+    this._form = this._popup.querySelector('.popup__container_type_form');
+    const inputs = Array.from(this._form.elements);
+    inputs.forEach(input => {
+      inputValues[input.name] = input.value;
+    })
+    return inputValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    const inputValues = this._getInputValues();
+    this._form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      this._submitCallback(inputValues);
+    })
+  }
+}
