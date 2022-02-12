f81cdf37a84bc75c4ed7e4769c7a2ba53c46ca44
Aleks P <aap-89@yandex.ru>
2022-02-12 21:25:25 +0300

    Исправил UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,16 +1,20 @@
 export default class UserInfo {
-  constructor(nameSelector, aboutSelector, {fetchInfoCallback, setInfoCallback}) {
+  constructor(nameSelector, aboutSelector, setUserCallback) {
     this._nameSelector = nameSelector;
     this._aboutSelector = aboutSelector;
-    this._fetchInfoCallback = fetchInfoCallback;
-    this._setInfoCallBack = setInfoCallback;
+    this._setUserCallback = setUserCallback;
   }
 
-  async getUserInfo() {
-    return await this._fetchInfoCallback();
+  getUserInfo(fetchedUser) {
+    this._user = fetchedUser;
+    return this._user;
   }
 
-  setUserInfo() {
-    this._setInfoCallBack(this._user);
+  setUserInfo({name, about}) {
+    this._setUserCallback({name, about});
+    const nameElement = document.querySelector(this._nameSelector);
+    const aboutElement = document.querySelector(this._aboutSelector);
+    nameElement.textContent = name;
+    aboutElement.textContent = about;
   }
 }
