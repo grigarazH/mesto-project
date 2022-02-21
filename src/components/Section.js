f5130ebf8cfc4e3722d2f367f65632f5cee3dfb7
grigarazH <45718270+grigarazH@users.noreply.github.com>
2022-02-21 22:00:48 +0300

    Исправил класс Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,11 +3,11 @@ export default class Section {
   constructor({renderer}, selector) {
     this._renderer = renderer;
     this._selector = selector;
+    this._element = document.querySelector(this._selector);
   }
 
   // Осуществляет генерацию нескольких элементов с помощью рендер-функции и добавление их в верстку
   renderItems(items) {
-    this._element = document.querySelector(this._selector);
     items.forEach(item => {
       const itemElement = this._renderer(item);
       this._element.append(itemElement);
@@ -17,13 +17,11 @@ export default class Section {
   // Осуществляет генерацию одного элемента с помощью рендер-функции и добавления его в верстку
   addItem(item) {
     const itemElement = this._renderer(item);
-    this._element = document.querySelector(this._selector);
     this._element.prepend(itemElement);
   }
 
   // Осуществляет удаление элемента по индексу и селектору из секции
   delete(index, selector) {
-    this._element = document.querySelector(this._selector);
     const elements = this._element.querySelectorAll(selector);
     elements[index].remove();
   }
