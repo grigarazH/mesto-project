ee28a3352bdb3a75b2f6a87b16ae5e9d2e1e7784
grigarazH <45718270+grigarazH@users.noreply.github.com>
2022-02-21 21:54:53 +0300

    Реализовал поиск элемента формы в конструкторе PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,11 +4,11 @@ import Popup from "./Popup";
 export default class PopupWithForm extends Popup {
   constructor(selector, submitCallback) {
     super(selector);
+    this._form = this._popup.querySelector('.popup__container_type_form');
     this._submitCallback = submitCallback;
   }
 
   close() {
-    this._form = this._popup.querySelector('.popup__container_type_form');
     this._form.reset();
     super.close();
   }
@@ -16,8 +16,6 @@ export default class PopupWithForm extends Popup {
   // Получает значения полей ввода
   _getInputValues() {
     const inputValues = {};
-    this._popup = document.querySelector(this._selector);
-    this._form = this._popup.querySelector('.popup__container_type_form');
     const inputs = Array.from(this._form.elements);
     inputs.forEach(input => {
       inputValues[input.name] = input.value;
@@ -27,7 +25,6 @@ export default class PopupWithForm extends Popup {
 
   setEventListeners() {
     super.setEventListeners();
-    this._form = this._popup.querySelector('.popup__container_type_form');
     this._form.addEventListener('submit', (event) => {
       event.preventDefault();
       const inputValues = this._getInputValues();
